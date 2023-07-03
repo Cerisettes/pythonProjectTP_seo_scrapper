f73e60c30c3935cf40e9b085d4d833380faab8f3
your name <you@example.com>
2023-07-03 11:50:33 +0200

    # Première version
    Mise en place du prétraitement du contenu. Connexion Python/MongoDB.

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Envoyer une requête HTTP à l'URL souhaitée
+response = requests.get('https://fr.wikipedia.org/wiki/France')
 
+# Analyser le contenu HTML de la réponse
+soup = BeautifulSoup(response.text, 'html.parser')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Extraire les balises de titre
+title_tags = soup.find_all(['title', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
 
+# Extraire les balises d'emphase
+emphasis_tags = soup.find_all(['b', 'strong', 'em'])
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Extraire les URL de liens
+link_tags = soup.find_all('a')
+links = [tag['href'] for tag in link_tags if 'href' in tag.attrs]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Récupérer le contenu des balises de titre et d'emphase
+title_content = [tag.get_text().strip() for tag in title_tags]
+emphasis_content = [tag.get_text().strip() for tag in emphasis_tags]
+
+# Stocker les méta-données dans MongoDB
+client = MongoClient('mongodb://localhost:27017')
+db = client['TPscrapiong']
+collection = db['exemple']
+
+# Insérer les méta-données dans la collection
+collection.insert_one({
+    'url': 'https://fr.wikipedia.org/wiki/France',
+    'title': title_content,
+    'emphasis': emphasis_content,
+    'links': links
+})
+
